60936a6db83113e7db63a5ce6ec78daa28748828
agdsarge <agdsargeant@gmail.com>
2020-05-05 16:22:04 -0400

    added a method to investors to return broker contact info

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -3,7 +3,9 @@ class Investor < ActiveRecord::Base
     has_many :companies, through: :transactions
     belongs_to :broker
 
-
-
+    def get_broker_contact
+        br_full_name = "#{self.broker.first_name} #{self.broker.last_name}"
+        "Your friendly trader is #{br_full_name}.\nTheir phone number is #{self.broker.telephone}.\n Their email address is #{self.broker.email}."
+    end
 
 end
